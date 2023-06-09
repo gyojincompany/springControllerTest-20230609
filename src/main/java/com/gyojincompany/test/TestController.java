71784bd377ce4fa378431b3c3581670a744472a8
gyojincompany <luckyred@gmail.com>
2023-06-09 14:20:34 +0900

    model and view 추가

--- src/main/java/com/gyojincompany/test/TestController.java
+++ src/main/java/com/gyojincompany/test/TestController.java
@@ -3,6 +3,7 @@ package com.gyojincompany.test;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
@@ -37,15 +38,31 @@ public class TestController {
 	public String memberinfo(Model model) {
 		
 		//원래는 DB에서 데이터를 가져옴
-		String memberId = "tiger";
-		String memberName = "홍길동";
-		int memberAge = 15;
+		String memberId = "whiteCat";
+		String memberName = "이순신";
+		int memberAge = 27;
 		
 		model.addAttribute("mid", memberId);
 		model.addAttribute("mname", memberName);
 		model.addAttribute("mage", memberAge);		
 		
 		return "memberinfo";		
+	}
+	
+	@RequestMapping(value = "/memberinfo2")
+	public ModelAndView memberinfo2(ModelAndView mv) {
+		
+		String memberId = "redDog";
+		String memberName = "김유신";
+		int memberAge = 19;
+		
+		mv.addObject("mid", memberId);
+		mv.addObject("mname", memberName);
+		mv.addObject("mage", memberAge);
+		
+		mv.setViewName("memberinfo");//실행하려는 jsp파일(view)의 이름까지 넣어 줌
+		
+		return mv;
 	}
 	
 	
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -10,7 +10,8 @@
 </h1>
 <a href="hello">hello.jsp</a><br><br>
 <a href="BlogHome.Naver">Naver Blog</a><br><br>
-<a href="memberinfo">Member Info View</a>
+<a href="memberinfo">Member Info View</a><br><br>
+<a href="memberinfo2">Member Info View2</a>
 
 </body>
 </html>
